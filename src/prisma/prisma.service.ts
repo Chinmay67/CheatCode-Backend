9fc3de7ef1f8d57e1cd7ce5a041dc61c13bc4097
Chinmay Singh <134195641+Chinmay67@users.noreply.github.com>
2025-01-14 16:59:15 +0530

    Update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient{
         super({
             datasources : {
                 db : {
-                    url : 'postgresql://neondb_owner:PQGchwy3X4LU@ep-odd-leaf-a1181nbr-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
+                    url : ''
                 }
             }
         });
